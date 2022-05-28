ecbeffe60dd831de91695ac74e7f8ef4a27208b7
Katarzyna Kleczkowska <katarzyna.k.kleczkowska@gmail.com>
2022-05-28 21:53:58 +0200

    Add files via upload

---
+++ Ćwiczenia_2_MongoDB_agregacje/zapytanie_1_A.js
@@ -0,0 +1,19 @@
+// Przed wykonaniem zadania dokonałam konwersji wagi i wzrostu ze stringów na liczby:
+// db.people.find().forEach( function (x) {x.weight = parseFloat(x.weight), x.height = parseFloat(x.height); db.people_conv.save(x);});
+// Można teoretycznie rozwiązać problem wstawiając $toDecimal, ale wtedy wyniki brzydko wyglądają :)
+
+printjson(db.people_conv.aggregate([
+    {$group: {
+        _id: "$sex",
+        average_weight: {$avg: "$weight"},
+        average_height: {$avg: "$height"}
+    }
+},
+{$project: {
+    "_id": 0, 
+    "płeć": "$_id", 
+    "średnia waga": {$trunc: ["$average_weight", 2]}, 
+    "średni wzrost" : {$trunc: ["$average_height", 2]}}
+}
+]
+).toArray())
